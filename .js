260f9d896063c7a8eb4b5bd4ef8dc04bb8defceb
Ana Marija Trajkovska <ana_marija_trajkovska@hotmail.com>
2020-12-13 14:06:55 +0100

    new

---
+++ .js
@@ -0,0 +1,24 @@
+function onClick(postId) {
+
+    document.getElementById("clicks" + postId).innerHTML = parseInt(document.getElementById("clicks" + postId).innerHTML) + 1
+};
+
+var name = window.prompt("Внесете го вашето име:");
+function myFunction(tmpId) {
+
+    komentar = document.getElementById("text"+tmpId).value;
+    if(komentar.length >0)
+    {
+        var nov_komentar = document.createElement("ul");
+        var t=new Date();
+        var time= t.getFullYear()
+     var x = time + " " + name + " : " + komentar ;
+     nov_komentar.innerHTML =x;
+    document.getElementById("myUL"+tmpId).appendChild(nov_komentar);
+    }
+    else{
+        alert("Text field empty!");
+        return false;
+    }
+    
+};
